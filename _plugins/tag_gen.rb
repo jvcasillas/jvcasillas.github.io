fff9812ea0e8463958126a821fa519a2d9be3a3d
joseph <jvcasill@email.arizona.edu>
2014-03-18 09:11:39 -0700

    new tags

--- _plugins/tag_gen.rb
+++ _plugins/tag_gen.rb
@@ -1,22 +1,18 @@
 module Jekyll
- 
   class TagIndex < Page
     def initialize(site, base, dir, tag)
       @site = site
       @base = base
       @dir = dir
       @name = 'index.html'
- 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
       self.data['tag'] = tag
       self.data['title'] = "Posts Tagged &ldquo;"+tag+"&rdquo;"
     end
   end
- 
   class TagGenerator < Generator
     safe true
-    
     def generate(site)
       if site.layouts.key? 'tag_index'
         dir = 'tags'
@@ -25,7 +21,6 @@ module Jekyll
         end
       end
     end
-  
     def write_tag_index(site, dir, tag)
       index = TagIndex.new(site, site.source, dir, tag)
       index.render(site.layouts, site.site_payload)
